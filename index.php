2fe153b8262548d24db6978eb369362d49806bce
KeanVatharo <47551637+KeanVatharo@users.noreply.github.com>
2019-08-21 13:49:34 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>User Login</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  </head>
+  <body>
+
+
+<div class="container">
+  <div class="login">
+    <div class="login-header"><h1>Login</h1></div>
+    <div class="login-body">
+      <form action="" autocomplete="off">
+        <label for="username">Username</label>
+        <input type="text" name="user" id="username">
+        <label for="password">Password</label>
+        <input type="password" name="pass" id="password">
+        <button type="submit" name="button">Login</button>
+      </form>
+    </div>
+  </div>
+</div>
+
+
+  </body>
+</html>
